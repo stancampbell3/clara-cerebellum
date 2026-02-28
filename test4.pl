d507dfd63b5a7947523f91681244d958b4a8ed23
Stan Campbell <stan.campbell3@gmail.com>
2026-02-28 10:51:24 -0800

    Woo fucking HOOO baby.

--- test4.pl
+++ test4.pl
@@ -4,3 +4,4 @@ use_module(library(the_coire)).
 man(stan).
 plan(stan).
 man_with_plan(Man) :- man(Man), plan(Man), coire_publish_assert(man_with_plan(Man)).
+get_out_the_back(Dude) :- man_with_plan(_), man(Dude), coire_publish_assert(get_out_the_back(Dude)).
