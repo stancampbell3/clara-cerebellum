16a2bec53724a424de67224290efa08c2e8955c2
Stan Campbell <stan.campbell3@gmail.com>
2026-03-04 09:58:33 -0800

    test5

--- test5.sh
+++ test5.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-curl -X POST http://localhost:8080/deduce/ -H "Content-Type: application/json" -d @deduce_request.json
+curl -X POST http://localhost:8080/deduce -H "Content-Type: application/json" -d @deduce_request.json
